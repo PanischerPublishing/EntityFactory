03f2d0bf9cbbd00a99572728964eeb2df9b05b71
Panischer <78178146+PanischerPublishing@users.noreply.github.com>
2021-01-31 01:05:53 +0100

    Add files via upload

---
+++ src/de/Panischer/listener/InfiniteDispensers.java
@@ -0,0 +1,29 @@
+package de.Panischer.listener;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Dispenser;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
+
+import de.Panischer.main.Main;
+public class InfiniteDispensers implements Listener
+{
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onBlockDispense(BlockDispenseEvent event)
+	{
+		if(event.getBlock().getType().equals(Material.DISPENSER))
+		{
+			Dispenser dispenser = (Dispenser) event.getBlock().getState();
+			Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(Main.plugin, new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					dispenser.getInventory().addItem(event.getItem());
+				}
+			});
+		}
+	}
+}
